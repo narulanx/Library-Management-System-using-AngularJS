f979cfb842bb56aca70cfc2e1c1c412aade0bd30
Nikhil Narula <nik.coolest@gmail.com>
2017-02-22 21:38:33 -0600

    feat: use of routings, templates/partials and custom services and filters

--- js/app.js
+++ js/app.js
@@ -1,2 +1,28 @@
 //declare main module and its dependencies
+var mainModule = angular.module('MainModule', ['libraryModule', 'serviceModule', 'filterModule']);
 
+mainModule.config(function($routeProvider) {
+	$routeProvider.when('/main', {
+		templateUrl: 'loginTemplate',
+		controller: 'LoginCtrl'
+	});
+	$routeProvider.when('/home/student', {
+		templateUrl: 'studentTemplate',
+		controller: 'BookListCtrl_Student'
+	});
+	$routeProvider.when('/home/librarian', {
+		templateUrl: 'librarianTemplate',
+		controller: 'BookListCtrl_Librarian'
+	});
+	$routeProvider.when('/issue/:bookId', {
+		templateUrl: 'issueBookTemplate',
+		controller: 'IssueBookCtrl'
+	});
+	$routeProvider.when('/return/:bookId', {
+		templateUrl: 'returnBookTemplate',
+		controller: 'ReturnBookCtrl'
+	});
+	$routeProvider.otherwise({
+		redirectTo: '/main'
+	});
+});
--- js/controllers.js
+++ js/controllers.js
@@ -7,47 +7,97 @@ var libraryModule = angular.module('libraryModule', []);
         Method1:
             validate(user): validates entered username and password and alerts appropriate messages
  */
-libraryModule.controller('LoginCtrl', function($scope, $http) {
+libraryModule.controller('LoginCtrl', function($scope, $http, $rootScope, $location) {
 	var url = "data/roles.json";
 	$http.get(url).success(function(response) {
 		$scope.data = response;
 	});
 
 	$scope.validate = function() {
-		var found = false;
+		$rootScope.username = "";
 		angular.forEach($scope.data, function(user) {
 			if (user.username == $scope.name && user.password == $scope.pass) {
-				if (!found) {
-					found = true;
-				}
+				$rootScope.username = $scope.name;
+				$scope.role = user.role;
 			}
 		});
-		if (found) {
-			window.alert("Login Successful!");
+		if ($rootScope.username != "") {
+			alert("Welcome " + $rootScope.username + "!");
+			if ($scope.role == "student") {
+				$location.path('/home/student');
+			} else {
+				$location.path('/home/librarian');
+			}
 		} else {
-			window.alert("Please provide valid login credentials.")
+			$location.path('/main');
 		}
 	}
 });
 
 /*
- * declare BookListCtrl
- * Code description for BookListCtrl:
-        Hard code book details in json array and store this in a books model
+ * Controller BookListCtrl_Student
  */
-libraryModule.controller('BookListCtrl_Student', function($scope, $http) {
+libraryModule.controller('BookListCtrl_Student', function($scope, BookData) {
+	BookData.getData();
+
 	$scope.title = "View Books";
-	var url = "data/books.json";
-	$http.get(url).success(function(response) {
-		$scope.bookDetails = response;
-	});
+
+	$scope.changeView = function() {
+		BookData.getData();
+	}
+});
+
+/*
+ * Controller BookListCtrl_Librarian
+ */
+libraryModule.controller('BookListCtrl_Librarian', function($scope, $location, $rootScope, BookData) {
+	if (!$rootScope.books) {
+		BookData.getData();
+	}
+
+	$scope.title = "View Books";
+
+	$scope.issue = function(bookId) {
+		$location.path('/issue/' + bookId);
+	}
+
+	$scope.return = function(bookId) {
+		$location.path('/return/' + bookId);
+	}
 });
 
 
-libraryModule.controller('BookListCtrl_Librarian', function($scope, $http) {
-	$scope.title = "View Books";
-	var url = "data/books.json";
-	$http.get(url).success(function(response) {
-		$scope.bookDetails = response;
+/*
+ * Controller IssueBookCtrl
+ */
+libraryModule.controller('IssueBookCtrl', function($scope, $routeParams, $rootScope, $location) {
+	$scope.bookId = $routeParams.bookId;
+	$scope.title = "Issue Books";
+	$scope.book = $rootScope.books.find(function(b) {
+		return b.bookId == $scope.bookId;
 	});
+
+	$scope.issue = function(bookId) {
+		$scope.book.issued = true;
+		alert("Book issued successfully!");
+		$location.path('/home/librarian');
+	}
+});
+
+
+/*
+ * Controller ReturnBookCtrl
+ */
+libraryModule.controller('ReturnBookCtrl', function($scope, $routeParams, $rootScope, $location) {
+	$scope.bookId = $routeParams.bookId;
+
+	$scope.book = $rootScope.books.find(function(b) {
+		return b.bookId == $scope.bookId;
+	});
+
+	$scope.return = function(bookId) {
+		$scope.book.issued = false;
+		alert("Book returned successfully!");
+		$location.path('/home/librarian');
+	}
 });
